a360d329760411a7494a612d9e7c80c711d48fbc
yunabc <450387582@qq.com>
2017-04-22 00:06:57 +0800

    add two folders

---
+++ src/components/page-components/login.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="login">
+		
+	</div>
+</template>
+<script>
+export default {
+	name: 'login',
+	data (){
+		return {
+
+		}
+	}
+}
+</script> 
+<style scoped>
+	
+</style>
