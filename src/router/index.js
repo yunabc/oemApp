e09a3b85433300af45762e8bb6fc099ee9a29dd4
yunabc <450387582@qq.com>
2017-04-22 14:37:58 +0800

    change login.vue reset.css index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import login from 'components/page-components/login'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'login',
+      component: login
     }
   ]
 })
