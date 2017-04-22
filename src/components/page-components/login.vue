e7a4017edfb970b7426222e5b1dc24fe3a35388a
yunabc <450387582@qq.com>
2017-04-22 15:48:51 +0800

    add lostpassword.vue register.vue registernext.vue commoncolor.less

--- src/components/page-components/login.vue
+++ src/components/page-components/login.vue
@@ -4,12 +4,13 @@
 			<input type="text" name="phone" placeholder="请输入手机号">
 			<input type="password" name="password" placeholder="请输入手机号">
 		</div>
-		<button class="submit-btn">登录</button>
+		<button class="btn submit-btn">登录</button>
 		<div class="register-forget clearfix">
-			<router-link class="fl" to="/goods">忘记密码</router-link>
-			<router-link class="fr" to="/goods">注册账号</router-link>
+			<router-link class="fl" to="/lostpassword">忘记密码</router-link>
+			<router-link class="fr" to="/register">注册账号</router-link>
 		</div>
 	</div>
+	<view-router></view-router>
 </template>
 <script>
 export default {
@@ -21,28 +22,7 @@ export default {
 	}
 }
 </script> 
-<style scoped>
-	.login{
-		padding: .8rem .53333333rem;
-		.inputbox{
-			padding-bottom: .4rem;
-			input{
-				display: inline-block;
-				border: .01333333rem solid #c9c9c9;
-				height: 66px;
-				line-height: 66px;
-				font-size: 24px;
-				color: #000;
-				padding-left: .26666667rem;
-			}
-		}
-		.submit-btn{
-			background-color: #6ce09e;
-			color: #fff;
-			font-size: 28px;
-			line-height: 68px;
-			line-height: 68px;
-			text-align: center;
-		}
-	}
+<style scoped lang="less" rel="stylesheet/less">
+	@import "../../common/style/login.less";
+	
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import login from 'components/page-components/login'
 import Hello from 'components/Hello'
 import Home from 'components/page-components/home'
+import lostpassword from 'components/page-components/lostpassword'
+import register from 'components/page-components/register'
+import registernext from 'components/page-components/registernext'
 
 
 Vue.use(Router)
@@ -11,12 +14,25 @@ export default new Router({
   routes: [
     {
       path: '/',
-
       name: 'login',
-      component: login
-
-      name: 'Hello',
-      component: Hello
+      component: login,
+      children:[
+        {
+          path:'/lostpassword',
+          name:'lostpassword',
+          component:lostpassword
+        },
+        {
+          path:'/register',
+          name:'register',
+          component:register,
+          children:[{
+            path:'/next',
+            name:'registernext',
+            component:registernext
+          }]
+        }
+      ]
     },
     {
       path:'/home',
