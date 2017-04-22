76decf0dce3f7c62f3963f66fead8270a4a7e97c
yunabc <450387582@qq.com>
2017-04-22 15:52:13 +0800

    add commoncolor.less

---
+++ src/components/page-components/lostpassword.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="login">
+		<div class="inputbox">
+			<div class="send-code">
+				<input type="text" name="phone" placeholder="请输入手机号">
+				<span class="send">发送验证码</span>
+			</div>
+			
+			<input type="password" name="password" placeholder="请输入手机号">
+		</div>
+		<button class="btn submit-btn">登录</button>
+		
+	</div>
+</template>
+<script>
+export default {
+	name: 'lostpassword',
+	data (){
+		return {
+
+		}
+	}
+}
+</script> 
+<style scoped lang="less" rel="stylesheet/less">
+	@import "../../common/style/login.less";
+	.login{
+		inputbox{
+			.send-code{
+				display: flex;
+				position: relative;
+				input{
+					display: inline-block;
+					width: 100%;
+					border: .01333333rem solid #c9c9c9;
+					height: 66px;
+					line-height: 66px;
+					font-size: 24px;
+					color: #000;
+					padding-left: .26666667rem;
+					margin-bottom: .4rem;
+					box-sizing: border-box;
+				}
+			}
+
+		}
+	}
+</style>
---
+++ src/components/page-components/register.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="login">
+		<div class="inputbox">
+			<input type="text" name="phone" placeholder="请输入手机号">
+			<input type="password" name="password" placeholder="请输入手机号">
+		</div>
+		<button class="btn submit-btn">登录</button>
+		<div class="register-forget clearfix">
+			<router-link class="fl" to="/goods">忘记密码</router-link>
+			<router-link class="fr" to="/goods">注册账号</router-link>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'login',
+	data (){
+		return {
+
+		}
+	}
+}
+</script> 
+<style scoped lang="less" rel="stylesheet/less">
+	@import "../../common/style/login.less";
+	
+</style>
