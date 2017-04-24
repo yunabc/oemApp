952a5c9c5581abc821732c5ddc8336996be9f3ec
wujincun <1769453779@qq.com>
2017-04-24 10:42:44 +0800

    轮播swiper

--- src/components/page-components/home.vue
+++ src/components/page-components/home.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="home">
-    <div class="home-banner">
-      <div class="swiper-slide"><img src="../../common/img/banner.jpg" alt="" width="100%"></div>
+    <div class="swiper-container home-banner" >
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="../../common/img/banner.jpg" alt="" width="100%"></div>
+        <div class="swiper-slide">
+          <img class="imgload" src="http://mooc.inxedu.com/images/upload/image/20151026/1446026905031.jpg" alt="首页banner图片02">
+        </div>
+        <div class="swiper-slide">Slide 3</div>
+      </div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+
+      <!-- 如果需要导航按钮 -->
+     <!-- <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>-->
     </div>
     <ul class="home-content">
       <li class="currentInvest investLi">
         <div class="infoDesc">
-          <img src="../../common/img/current.png" alt="" >
+          <img src="../../common/img/current.png" alt="">
           <div class="desc">
             <a href="" class="title">活期理财</a>
             <div class="subtitle">随存随取 复利计息</div>
@@ -41,89 +53,112 @@
 
 <script>
   import footNav from 'components/common-components/footNav';
+  import Swiper from 'swiper';
+
   export default {
     data () {
       return {}
     },
     components:{
       footNav
-    }
+    },
+     mounted () {
+       var mySwiper = new Swiper('.swiper-container', {
+         direction: 'horizontal',
+         loop: true,
+         autoplay:'3000',
+         pagination: '.swiper-pagination',
+         nextButton: '.swiper-button-next',
+         prevButton: '.swiper-button-prev'
+       })
+     },
   }
+
 </script>
 
 <style lang="less" rel="stylesheet/less">
   @import '../../common/style/commoncolor.less';
-  #home{
+  @import '../../../static/css/swiper-3.4.2.min.css';
+
+  #home {
     background-color: @backGrayColor;
     height: 100%;
-    .home-content{
+    .home-banner{
+      height: 360px;
+      /*.swiper-slide{
+        img{
+          width: 100%;
+        }
+      }*/
+    }
+    .home-content {
       padding: 0 .4rem;
-      .investLi{
+      .investLi {
         background-color: #fff;
         margin-top: .4rem;
-        border-radius:.13333333rem;
-        &:active{
+        border-radius: .13333333rem;
+        &:active {
           background-color: @backGrayColor;
-         }
-        .infoDesc{
-          display:flex;
+        }
+        .infoDesc {
+          display: flex;
           padding: .50666667rem .66666667rem .4rem;
-          img{
+          img {
             width: .78666667rem;
             height: .77333333rem;
             margin-top: .21333333rem;
           }
-          .desc{
-            margin-left:.64rem;
-            .title{
+          .desc {
+            margin-left: .64rem;
+            .title {
               font-size: .42666667rem;
               line-height: .48rem;
               font-weight: bold;
               margin-bottom: .32rem;
             }
-            .subtitle{
+            .subtitle {
               font-size: .32rem;
               line-height: .4rem;
               color: #666666;
             }
           }
         }
-        .deviLine{
+        .deviLine {
           border-top: .01333333rem solid @backGrayColor;
-          position:relative;
-          margin:.06666667rem 0;
-          &:before,&:after{
-            display:block;
-            content:"";
-            position:absolute;
+          position: relative;
+          margin: .06666667rem 0;
+          &:before, &:after {
+            display: block;
+            content: "";
+            position: absolute;
             width: .13333333rem;
             height: .26666667rem;
             background-color: @backGrayColor;
-            top:-.13333333rem;
+            top: -.13333333rem;
           }
-          &:before{
+          &:before {
             left: 0;
-             border-radius: 0 .13333333rem .13333333rem 0;
+            border-radius: 0 .13333333rem .13333333rem 0;
           }
-          &:after{
-             right: 0;
-             border-radius: .13333333rem 0 0 .13333333rem;
-           }
+          &:after {
+            right: 0;
+            border-radius: .13333333rem 0 0 .13333333rem;
+          }
         }
-        .infoCharge{
+        .infoCharge {
           display: flex;
           padding: .22666667rem .66666667rem;
-          justify-content:space-between;
-          line-height:.64rem;
-          .name{
+          justify-content: space-between;
+          line-height: .64rem;
+          .name {
             font-size: .32rem;
             color: #333333;
           }
-          .profit{
+          .profit {
             font-size: .42666667rem;
             color: @color;
           }
-          .buyBtn{
+          .buyBtn {
             width: 1.6rem;
             height: .64rem;
             border-radius: .32rem;
