e8aebc84ac7653e77d5a9616b123cab063c75bf6
wujincun <1769453779@qq.com>
2017-04-28 15:57:31 +0800

    investHeader css

--- src/components/page-components/currentProduct.vue
+++ src/components/page-components/currentProduct.vue
@@ -152,43 +152,7 @@
   @import '../../common/style/commoncolor.less';
 
   #product {
-    .investHeader {
-      display: flex;
-      flex-flow: column;
-      align-items: center;
-      padding: .53333333rem 0;
-      background-color: #fff;
-      margin-bottom: .26666667rem;
-      div {
-        margin-bottom: .53333333rem;
-      }
-      .title {
-        font-size: .37333333rem;
-        line-height: .37333333rem;
-      }
-      .profit {
-        font-size: .64rem;
-        line-height: .64rem;
-        margin-bottom: .26666667rem;
-      }
-      .times {
-        font-size: .26666667rem;
-        .days {
-          font-size: .32rem;
-          color: red;
-        }
-      }
-      .buyBtn {
-        width: 6.4rem;
-        height: .90666667rem;
-        border: .04rem solid @color;
-        text-align: center;
-        font-size: .37333333rem;
-        color: @color;
-        line-height: .90666667rem;
-        border-radius: .45333333rem /* 34px */;
-      }
-    }
+
     .clockHint {
       display: flex;
       display: none;
